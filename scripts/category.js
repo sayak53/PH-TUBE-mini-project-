932c15766120111556fb5bca00eec29e9a46b553
sayak53 <sayakchakraborty928@gmail.com>
2024-10-02 21:12:02 +0530

    done for today no more energy

--- scripts/category.js
+++ scripts/category.js
@@ -5,6 +5,15 @@ const loadCategories = () => {
     .then(data => displayCategories(data.categories))
     .catch(err => console.log(err))
 }
+
+// loadVideos function
+const loadvideos = () => {
+    fetch('https://openapi.programming-hero.com/api/phero-tube/videos')
+    .then(res => res.json())
+    .then(data => DisplayVideos(data.videos))
+    .catch(err => console.log(err))
+}
+
 // DisplayCategories function
 const displayCategories = (categories) => {
     const navCategory = document.getElementById('category')
@@ -16,4 +25,32 @@ const displayCategories = (categories) => {
         navCategory.append(btn)
     });
 }
-loadCategories()
+
+// DisplayVideos function
+const DisplayVideos = (videos) => {
+    const vid =document.getElementById('videos')
+    videos.forEach(video => {
+        console.log(video)
+
+        const div =document.createElement('div')
+        div.innerHTML=`
+        <figure class="h-[200px]">
+      <img
+        src=${video.thumbnail}
+        class="h-full w-full object-cover"
+        alt="Shoes" />
+     </figure>
+     <div class="card-body">
+       <h2 class="card-title">Shoes!</h2>
+       <p>If a dog chews shoes whose shoes does he choose?</p>
+       <div class="card-actions justify-end">
+         <button class="btn btn-primary">Buy Now</button>
+       </div>
+     </div>
+        `
+        div.classList ='card card-compact'
+        vid.append(div)
+    })
+}
+loadCategories();
+loadvideos();
